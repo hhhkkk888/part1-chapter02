67bbd09902c6b3272a0ab0f4a892bebe7044cfec
hhhkkk888 <gyeonghwan06@gmail.com>
2021-07-26 12:54:11 +0900

    초기 커밋.

---
+++ part1-chapter02/part1-chapter02.cpp
@@ -0,0 +1,91 @@
+﻿/*
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int num = 10;
+	int i = 0;
+
+	cout << "true: " << true << endl;
+	cout << "false: " << false << endl;
+
+	while (true)
+	{
+		cout << i++ << ' ';
+		if (i > num)
+			break;
+	}
+	cout << endl;
+
+	cout << "sizeof 1: " << sizeof(1) << endl;
+	cout << "sizeof 0: " << sizeof(0) << endl;
+	cout << "sizeof true: " << sizeof(true) << endl;
+	cout << "sizeof false: " << sizeof(false) << endl;
+	return 0;
+
+}
+
+
+#include <iostream>
+
+bool ispositive(int num)
+{
+	if (num <= 0)
+		return false;
+	else
+		return true;
+}
+
+int main(void)
+{
+	bool ispos;
+	int num;
+	std::cout << "input number: ";
+	std::cin >> num;
+
+	ispos = ispositive(num);
+
+	if (ispos)
+		std::cout << "positive number" << std::endl;
+	else
+		std::cout << "negative number" <<std::endl;
+
+	return 0;
+
+}
+
+#include <iostream>
+
+
+int main(void)
+{
+	int num1 = 1020;
+	int& num2 = num1;//참조자
+
+	num2 = 3047;
+	
+	std::cout << "VAL: " << num1 << std::endl;//같은 값
+	std::cout << "REF: " << num2 << std::endl;//같은 값
+
+	std::cout << "VAL: " << &num1 << std::endl;//같은 주소값
+	std::cout << "REF: " << &num2 << std::endl;//같은 주소값
+
+	return 0;
+}
+
+#include <iostream>
+
+int main(void)
+{
+	int arr[3] = { 1,3,5 };
+	int& ref1 = arr[0];
+	int& ref2 = arr[1];
+	int& ref3 = arr[2];
+
+	std::cout << ref1 << std::endl;
+	std::cout << ref2 << std::endl;
+	std::cout << ref3 << std::endl;
+	return 0;
+}*/
